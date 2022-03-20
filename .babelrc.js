ed72375fc13ddf3d17ab785bdaa673a3f825c217
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-03-20 18:37:30 +0530

    Update .babelrc.js

--- .babelrc.js
+++ .babelrc.js
@@ -3,6 +3,7 @@ const env = require('./env-config');
 module.exports = {
 	"presets": ["next/babel"],
 	"plugins": [
+		["@babel/plugin-transform-runtime"],
 		["styled-components", { "ssr": true }],
 		["transform-define", env]
 	]
