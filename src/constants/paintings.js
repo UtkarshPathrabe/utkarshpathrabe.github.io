1d5d03cd8079d73e18dfcf51f137e13912af2623
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-03-20 18:04:18 +0530

    Create paintings.js

---
+++ src/constants/paintings.js
@@ -0,0 +1,97 @@
+export const paintings = [
+    {
+        height: 231,
+        width: 170,
+        src: '/images/paintings/coconut.png',
+    },
+    {
+        height: 274,
+        width: 179,
+        src: '/images/paintings/ixora-singapuri-red-flower.png',
+    },
+    {
+        height: 1,
+        width: 1,
+        src: '/images/paintings/design.png',
+    },
+    {
+        height: 49,
+        width: 37,
+        src: '/images/paintings/free-hand-drawing-vase.png',
+    },
+    {
+        height: 553,
+        width: 404,
+        src: '/images/paintings/free-hand-drawing.png',
+    },
+    {
+        height: 248,
+        width: 219,
+        src: '/images/paintings/bucket.png',
+    },
+    {
+        height: 194,
+        width: 439,
+        src: '/images/paintings/tomatoes.png',
+    },
+    {
+        height: 283,
+        width: 564,
+        src: '/images/paintings/lamp-design.png',
+    },
+    {
+        height: 1,
+        width: 1,
+        src: '/images/paintings/design-02.jpg',
+    },
+    {
+        height: 1,
+        width: 1,
+        src: '/images/paintings/design-04.jpg',
+    },
+    {
+        height: 1,
+        width: 1,
+        src: '/images/paintings/design-05.jpg',
+    },
+    {
+        height: 1,
+        width: 1,
+        src: '/images/paintings/design-06.jpg',
+    },
+    {
+        height: 1,
+        width: 1,
+        src: '/images/paintings/design-07.jpg',
+    },
+    {
+        height: 1,
+        width: 1,
+        src: '/images/paintings/design-01.jpg',
+    },
+    {
+        height: 1,
+        width: 1,
+        src: '/images/paintings/design-03.jpg',
+    },
+    {
+        height: 142,
+        width: 165,
+        src: '/images/paintings/elephant-free-hand-drawing.jpg',
+    },
+    {
+        height: 481,
+        width: 422,
+        src: '/images/paintings/sadafuli-flower.png',
+    },
+    {
+        height: 3,
+        width: 4,
+        src: '/images/paintings/still-life.jpg',
+    },
+    {
+        height: 3,
+        width: 4,
+        src: '/images/paintings/paper-vendor.jpg',
+    },
+];
