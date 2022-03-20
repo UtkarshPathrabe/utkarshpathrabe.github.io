8098ba99912695e8307200499ecba1bc225f8a25
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-03-20 18:03:45 +0530

    Create Technologies.js

---
+++ src/components/Technologies/Technologies.js
@@ -0,0 +1,287 @@
+import React from 'react';
+import { DiBootstrap, DiCss3, DiDatabase, DiGit, DiHeroku, DiHtml5, DiJava, DiJsBadge, DiJira, DiLinux, DiMarkdown, DiMongodb, DiMysql, DiPostgresql, DiPython, DiReact, DiRedis, DiWindows } from 'react-icons/di';
+import { SiBabel, SiBitbucket, SiConfluence, SiCplusplus, SiDjango, SiEclipseide, SiEslint, SiGithub, SiGitlab, SiIntellijidea, SiJenkins, SiKeras, SiLeaflet, SiMaterialUi, SiMocha, SiNodeDotJs, SiNpm, SiNetlify, SiOctave, SiPostman, SiR, SiRedux, SiSocketDotIo, SiSpring, SiSublimetext, SiTypescript, SiVisualstudiocode } from 'react-icons/si';
+import { SectionText } from '../../styles/GlobalComponents';
+import { List, ListContainer, ListItem, ListTitle } from './TechnologiesStyles';
+
+const Technologies = () =>  (
+  <>
+    <SectionText>
+      {`I've worked on a range of technologies and tools in the software and web development world, be it documentation, work and task tracking, front-end and back-end code development, cloud technology, artificial intelligence and so on...`}
+    </SectionText>
+    <List>
+      <ListItem>
+        <SiBabel size='3rem' />
+        <ListContainer>
+          <ListTitle>Babel</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiBitbucket size='3rem' />
+        <ListContainer>
+          <ListTitle>BitBucket</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiBootstrap size='3rem' />
+        <ListContainer>
+          <ListTitle>Bootstrap</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiConfluence size='3rem' />
+        <ListContainer>
+          <ListTitle>Confluence</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiCplusplus size='3rem' />
+        <ListContainer>
+          <ListTitle>C++</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiCss3 size='3rem' />
+        <ListContainer>
+          <ListTitle>CSS 3</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiDatabase size='3rem' />
+        <ListContainer>
+          <ListTitle>Database</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiDjango size='3rem' />
+        <ListContainer>
+          <ListTitle>Django</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiEclipseide size='3rem' />
+        <ListContainer>
+          <ListTitle>Eclipse IDE</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiEslint size='3rem' />
+        <ListContainer>
+          <ListTitle>ESLint</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiGit size='3rem' />
+        <ListContainer>
+          <ListTitle>Git</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiGithub size='3rem' />
+        <ListContainer>
+          <ListTitle>GitHub</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiGitlab size='3rem' />
+        <ListContainer>
+          <ListTitle>Gitlab</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiHeroku size='3rem' />
+        <ListContainer>
+          <ListTitle>Heroku</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiHtml5 size='3rem' />
+        <ListContainer>
+          <ListTitle>HTML 5</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiIntellijidea size='3rem' />
+        <ListContainer>
+          <ListTitle>IntellijIdea</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiJava size='3rem' />
+        <ListContainer>
+          <ListTitle>Java</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiJsBadge size='3rem' />
+        <ListContainer>
+          <ListTitle>JavaScript</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiJenkins size='3rem' />
+        <ListContainer>
+          <ListTitle>Jenkins</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiJira size='3rem' />
+        <ListContainer>
+          <ListTitle>Jira</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiKeras size='3rem' />
+        <ListContainer>
+          <ListTitle>Keras</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiLeaflet size='3rem' />
+        <ListContainer>
+          <ListTitle>Leaflet</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiLinux size='3rem' />
+        <ListContainer>
+          <ListTitle>Linux</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiMarkdown size='3rem' />
+        <ListContainer>
+          <ListTitle>Markdown</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiMaterialUi size='3rem' />
+        <ListContainer>
+          <ListTitle>MaterialUI</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiMocha size='3rem' />
+        <ListContainer>
+          <ListTitle>MochaJS</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiMongodb size='3rem' />
+        <ListContainer>
+          <ListTitle>MongoDB</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiMysql size='3rem' />
+        <ListContainer>
+          <ListTitle>MySQL</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiNetlify size='3rem' />
+        <ListContainer>
+          <ListTitle>Netlify</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiNodeDotJs size='3rem' />
+        <ListContainer>
+          <ListTitle>NodeJS</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiNpm size='3rem' />
+        <ListContainer>
+          <ListTitle>NPM</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiOctave size='3rem' />
+        <ListContainer>
+          <ListTitle>Octave</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiPostgresql size='3rem' />
+        <ListContainer>
+          <ListTitle>PostgreSQL</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiPostman size='3rem' />
+        <ListContainer>
+          <ListTitle>Postman</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiPython size='3rem' />
+        <ListContainer>
+          <ListTitle>Python</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiR size='3rem' />
+        <ListContainer>
+          <ListTitle>R</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiReact size='3rem' />
+        <ListContainer>
+          <ListTitle>ReactJS</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiRedis size='3rem' />
+        <ListContainer>
+          <ListTitle>Redis</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiRedux size='3rem' />
+        <ListContainer>
+          <ListTitle>Redux</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiSocketDotIo size='3rem' />
+        <ListContainer>
+          <ListTitle>Socket.io</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiSpring size='3rem' />
+        <ListContainer>
+          <ListTitle>Spring</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiSublimetext size='3rem' />
+        <ListContainer>
+          <ListTitle>Sublime</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiTypescript size='3rem' />
+        <ListContainer>
+          <ListTitle>TypeScript</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiVisualstudiocode size='3rem' />
+        <ListContainer>
+          <ListTitle>Visual Studio Code</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiWindows size='3rem' />
+        <ListContainer>
+          <ListTitle>Windows</ListTitle>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </>
+);
+
+export default Technologies;
