ebf9393e4d5ff6b11d220c589c6ae480c11d13c4
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-03-20 18:02:54 +0530

    Create BackdropStyles.js

---
+++ src/components/Backdrop/BackdropStyles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const BackdropContainer = styled.div`
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.7);
+    top: 0;
+    left: 0;
+    z-index: 100;
+`;
