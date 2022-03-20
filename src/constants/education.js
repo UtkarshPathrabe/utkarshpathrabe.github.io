75f2cffb5904dfba365c898128df272e7a78e9ca
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-03-20 18:04:04 +0530

    Create education.js

---
+++ src/constants/education.js
@@ -0,0 +1,7 @@
+const EducationData = [
+    { year: 2010, timeLine: 'Mar 2010 (Class X)', text: `St. John's High School, Mohan Nagar, Nagpur.`, board: 'Maharashtra State Board of Secondary & Higher Secondary Education.', score: '95.27%' },
+    { year: 2012, timeLine: 'Feb 2012 (Class XII)', text: 'Shri Shivaji Science College, Congress Nagar, Nagpur.', board: 'Maharashtra State Board of Secondary & Higher Secondary Education.', score: '95%' },
+    { year: 2016, timeLine: '2012 - 2016', text: 'B.E. (Hons.) Computer Science', board: 'Birla Institute of Technology & Science, Pilani', score: '9.33 CGPA / 10' },
+];
+
+export default EducationData;
