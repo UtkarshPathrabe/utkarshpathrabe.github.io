bf76f6bb429e8036bdfa4355363f058d43e6a991
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-03-20 18:37:40 +0530

    Update Paintings.js

--- src/components/Paintings/Paintings.js
+++ src/components/Paintings/Paintings.js
@@ -1,13 +1,15 @@
 import Gallery from "react-photo-gallery";
-import { SectionText } from '../../styles/GlobalComponents';
+import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { paintings } from '../../constants/paintings';
 
 const Paintings = () => {
     return (
-        <>
+        <Section id="paintings">
+            <SectionDivider />
+            <SectionTitle main>Paintings</SectionTitle>
             <SectionText>Some of my paintings...</SectionText>
             <Gallery photos={paintings} />
-        </>
+        </Section>
     );
 };
 
