243a7d33a7268ae180f231adc37ccea3fb9ed91e
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-03-20 18:04:06 +0530

    Create experience.js

---
+++ src/constants/experience.js
@@ -0,0 +1,10 @@
+const ExperienceData = [
+    { year: 2014, timeLine: 'May 2014 - Jul 2014', text: 'Summer Intern at CMC Ltd., Mumbai', },
+    { year: 2015, timeLine: 'Jun 2015 - Jul 2015', text: 'Data Analyst Summer Intern at InfoCepts Technologies Pvt. Ltd., Nagpur', },
+    { year: 2016, timeLine: 'Jan 2016 - Jun 2016', text: 'Intern at J P Morgan Chase & Co., Mumbai', },
+    { year: 2016, timeLine: 'Jun 2016 - Jan 2020', text: 'Associate Software Engineer at J P Morgan Chase & Co., Bengaluru', },
+    { year: 2020, timeLine: 'Jan 2020 - Mar 2021', text: 'Senior Associate Software Engineer at J P Morgan Chase & Co., Bengaluru', },
+    { year: 2021, timeLine: 'Mar 2021 - Present', text: 'Senior System Software Engineer at NVIDIA Graphics Pvt. Ltd., Pune', },
+];
+
+export default ExperienceData;
