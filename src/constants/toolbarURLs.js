f82fa6c8d4f31ac7f54e4ec1c2fabc81403d987a
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-03-20 18:04:31 +0530

    Create toolbarURLs.js

---
+++ src/constants/toolbarURLs.js
@@ -0,0 +1,5 @@
+export const ABOUT_URL = '#about';
+export const PROJECTS_URL = '#projects';
+export const TECHNOLOGIES_URL = '#tech';
+export const ACCOMPLISHMENTS_URL = '#accomplishments';
+export const PAINTINGS_URL = '#paintings';
