83f2f6845834b8c51b4f5c68e4b636a817b579ec
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-03-20 18:04:22 +0530

    Create testScores.js

---
+++ src/constants/testScores.js
@@ -0,0 +1,24 @@
+const TestScores = [
+    {
+        title: 'BITSAT 2012',
+        score: '383/450',
+        date: 'May 2012'
+    },
+    {
+        title: 'IIT JEE 2012',
+        score: 'AIR 7627',
+        date: 'May 2012'
+    },
+    {
+        title: 'AIEEE 2012',
+        score: 'AIR 2842 & SR 255',
+        date: 'May 2012'
+    },
+    {
+        title: 'MHT-CET 2012',
+        score: 'SR 141 (183/200)',
+        date: 'May 2012'
+    }
+];
+
+export default TestScores;
