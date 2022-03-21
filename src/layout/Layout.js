a09f9256bbca64b7f2de02c981477e1a12920d40
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-03-21 23:13:41 +0530

    Update Layout.js

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,4 +1,4 @@
-import { useState, createContext, useEffect } from 'react';
+import { useState } from 'react';
 
 import Footer from '../components/Footer/Footer';
 import Toolbar from '../components/Toolbar/Toolbar';
@@ -8,46 +8,11 @@ import ScrollButton from '../components/ScrollButton/ScrollButton';
 import { Container, MainContainer } from './LayoutStyles';
 import { ABOUT_URL, ACCOMPLISHMENTS_URL, PAINTINGS_URL, PROJECTS_URL, TECHNOLOGIES_URL } from "../constants/toolbarURLs.js";
 
-export const AccordionContext = createContext(null);
-
 const Layout = ({children}) => {
   const [sideDrawerOpen, setSideDrawerOpen] = useState(false);
-  const [projectsIsOpen, setProjectsIsOpen] = useState(false);
-  const [technologiesIsOpen, setTechnologiesIsOpen] = useState(false);
-  const [accomplishmentsIsOpen, setAccomplishmentsIsOpen] = useState(false);
-  const [paintingsIsOpen, setPaintingsIsOpen] = useState(false);
-
-  useEffect(() => {
-    const hash = window.location.hash;
-    if (hash === PROJECTS_URL) {
-      setProjectsIsOpen(true);
-    } else if (hash === TECHNOLOGIES_URL) {
-      setTechnologiesIsOpen(true);
-    } else if (hash === ACCOMPLISHMENTS_URL) {
-      setAccomplishmentsIsOpen(true);
-    } else if (hash === PAINTINGS_URL) {
-      setPaintingsIsOpen(true);
-    }
-  }, []);
 
   const drawerToggleClickHandler = () => {
     setSideDrawerOpen(current => !current);
-  };
-
-  const toggleProjectsIsOpen = () => {
-    setProjectsIsOpen(current => !current);
-  };
-
-  const toggleTechnologiesIsOpen = () => {
-    setTechnologiesIsOpen(current => !current);
-  };
-
-  const toggleAccomplishmentsIsOpen = () => {
-    setAccomplishmentsIsOpen(current => !current);
-  };
-
-  const togglePaintingsIsOpen = () => {
-    setPaintingsIsOpen(current => !current);
   };
 
   const backdropClickHandler = () => {
@@ -59,46 +24,17 @@ const Layout = ({children}) => {
     backdrop = (<Backdrop onClickHandler={backdropClickHandler} />);
   }
 
-  const updateContextValue = (contextValue, { projectsOpen=false, technologiesOpen=false, accomplishmentsOpen=false, paintingsOpen=false }) => {
-    contextValue.setProjectsIsOpen(projectsOpen);
-    contextValue.setTechnologiesIsOpen(technologiesOpen);
-    contextValue.setAccomplishmentsIsOpen(accomplishmentsOpen);
-    contextValue.setPaintingsIsOpen(paintingsOpen);
-    backdropClickHandler();
-  };
-
-  const handleLinkClick = ({ contextValue, url }) => {
-    if (url === ABOUT_URL) {
-      updateContextValue(contextValue, {});
-    } else if (url === PROJECTS_URL) {
-      updateContextValue(contextValue, { projectsOpen: true });
-    } else if (url === TECHNOLOGIES_URL) {
-      updateContextValue(contextValue, { technologiesOpen: true });
-    } else if (url === ACCOMPLISHMENTS_URL) {
-      updateContextValue(contextValue, { accomplishmentsOpen: true });
-    } else if (url === PAINTINGS_URL) {
-      updateContextValue(contextValue, { paintingsOpen: true });
-    }
-  };
-
   return (
-    <AccordionContext.Provider value={{ 
-      projectsIsOpen, toggleProjectsIsOpen, setProjectsIsOpen,
-      technologiesIsOpen, toggleTechnologiesIsOpen, setTechnologiesIsOpen,
-      accomplishmentsIsOpen, toggleAccomplishmentsIsOpen, setAccomplishmentsIsOpen,
-      paintingsIsOpen, togglePaintingsIsOpen, setPaintingsIsOpen,
-    }}>
-      <Container>
-        <Toolbar toggleButtonHandler={drawerToggleClickHandler} handleLinkClick={handleLinkClick} />
-        <SideDrawer isOpen={sideDrawerOpen} itemClickHandler={backdropClickHandler} handleLinkClick={handleLinkClick} />
-        {backdrop}
-        <MainContainer>
-          {children}
-        </MainContainer>
-        <Footer/>
-        <ScrollButton />
-      </Container>
-    </AccordionContext.Provider>
+    <Container>
+      <Toolbar toggleButtonHandler={drawerToggleClickHandler} />
+      <SideDrawer isOpen={sideDrawerOpen} itemClickHandler={backdropClickHandler} />
+      {backdrop}
+      <MainContainer>
+        {children}
+      </MainContainer>
+      <Footer/>
+      <ScrollButton />
+    </Container>
   )
 };
 
