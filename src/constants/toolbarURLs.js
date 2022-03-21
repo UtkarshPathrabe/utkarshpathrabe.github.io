098e0e8622f889a90fbf680de19b4f9b511595b0
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-03-21 23:13:44 +0530

    Update toolbarURLs.js

--- src/constants/toolbarURLs.js
+++ src/constants/toolbarURLs.js
@@ -1,5 +1,5 @@
 export const ABOUT_URL = '#about';
-export const PROJECTS_URL = '#projects';
-export const TECHNOLOGIES_URL = '#tech';
-export const ACCOMPLISHMENTS_URL = '#accomplishments';
-export const PAINTINGS_URL = '#paintings';
+export const PROJECTS_URL = 'https://utkarshpathrabe.com/#projects';
+export const TECHNOLOGIES_URL = 'https://utkarshpathrabe.com/#tech';
+export const ACCOMPLISHMENTS_URL = 'https://utkarshpathrabe.com/#accomplishments';
+export const PAINTINGS_URL = 'https://utkarshpathrabe.com/#paintings';
