85c09cb8bc43a77bd687694de21b9981b72e3d6c
Utkarsh Ashok Pathrabe <uapathrabe@gmail.com>
2025-01-04 09:33:52 +0530

    Update toolbarURLs.js

--- src/constants/toolbarURLs.js
+++ src/constants/toolbarURLs.js
@@ -1,5 +1,5 @@
 export const ABOUT_URL = '#about';
 export const PROJECTS_URL = 'https://utkarshpathrabe.site/#projects';
-export const TECHNOLOGIES_URL = 'https://utkarshpathrabe.site/#tech';
+export const TECHNOLOGIES_URL = 'https://utkarshpathrabe.site/#skills';
 export const ACCOMPLISHMENTS_URL = 'https://utkarshpathrabe.site/#accomplishments';
 export const PAINTINGS_URL = 'https://utkarshpathrabe.site/#paintings';
